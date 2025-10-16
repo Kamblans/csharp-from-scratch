862167e76ed8a8328f152556d39050d04077b9b0
Kamblans <kamblans27@gmail.com>
2025-10-15 22:20:56 -0500

    Removing Using 2.0

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,6 +1,4 @@
-﻿using System.Reflection.Metadata.Ecma335;
-
-namespace HelloWorld{
+﻿namespace HelloWorld{
     class Program {
     static void Main(){
         const string Name="Carlos Mosquera";
