411a066899a95cef2a4b6728ff2dfee4c57fc291
Kamblans <kamblans27@gmail.com>
2025-10-15 22:16:23 -0500

    Hello World Project Changes

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,3 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.ReadLine();
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace HelloWorld{
+    class Program {
+    static void Main(){
+        const string Name="Carlos Mosquera";
+        Console.WriteLine($"!Hello world¡ { Name}");
+        Console.WriteLine();
+    }
+}
+}
